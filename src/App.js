82b68d3faf49128a00b7129dcba25eb963f394a2
Bruno Siqueira <santoserobinho@gmail.com>
2022-03-30 18:02:37 -0300

    2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+
+const MainContainer = styled.div `
+  display:  flex;
+  background-color: black;
+  min-height: 100vh;
+`
+const Filtros = styled.div`
+  background-color: green;
+  width: 22%;
+`
+
+const Produtos = styled.div `
+  background-color: red;
+  width: 56%;
+`
+
+const Carrinho = styled.div`
+  background-color: blue;
+  width: 22%;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainContainer>
+      <Filtros>
+        <h3>Filtros</h3>
+        <label>Valor Minimo: <input type="number" value="100"></input></label>
+        <label>Valor Maximo: <input type="number" value="1000"></input></label>
+        <label>Busca por nome: <input type="text"></input></label>
+      </Filtros>
+      <Produtos>
+        <h3>Rapaziada</h3>
+      </Produtos>
+      <Carrinho>
+        <h3>Olá</h3>
+      </Carrinho>
+
+
+    </MainContainer>
   );
 }
 
