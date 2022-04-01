663a8c93acca20feafbb21fa01914725f784477a
Bruno Siqueira <santoserobinho@gmail.com>
2022-04-01 12:50:19 -0300

    filtro e produtos terminados

--- src/App.js
+++ src/App.js
@@ -12,18 +12,92 @@ const MainContainer = styled.div`
   background-color: black;
   min-height: 100vh;
 `
+const products = [
+  {
+      id: 1,
+      name: 'Camiseta NASA Preta',
+      img: 'https://images2.imgbox.com/d1/e5/kF02pWQQ_o.jpg',
+      price: 150
+    },
 
-function App() {
+    {
+      id: 2,
+      name: 'Camiseta NASA Branca',
+      img: 'https://images2.imgbox.com/03/70/oX7P2WyD_o.jpg',
+      price: 180
+    },
 
-    
+    {
+      id: 3,
+      name: 'Camiseta Gato Astronauta ',
+      img: 'https://images2.imgbox.com/1d/e4/dqrUoqDq_o.jpg',
+      price: 250
+    },
+
+    {
+      id: 4,
+      name: 'Camiseta Planetas',
+      img: 'https://images2.imgbox.com/5a/be/IySszcVy_o.jpg',
+      price: 200
+    },
+
+    {
+      id: 5,
+      name: 'Camiseta "I need Space"',
+      img: 'https://images2.imgbox.com/af/e2/ABdnZE87_o.jpg',
+      price: 50
+    },
+
+    {
+      id: 6,
+      name: 'Camiseta Alien',
+      img: 'https://images2.imgbox.com/1e/9a/x6I45BAP_o.jpg',
+      price: 300
+    }
+]
+
+class App extends React.Component{
+  state = {
+    minFilter: 0,
+    maxFilter: 1000,
+    nameFilter: "",
+    productsInCart: []
+  }
+
+  onChangeMinFilter = (event) => {
+    this.setState({minFilter: event.target.value})
+  }
+
+  onChangeMaxFilter = (event) => {
+    this.setState({maxFilter: event.target.value})
+  }
+
+  onChangeNameFilter = (event) => {
+    this.setState({nameFilter: event.target.value})
+  }
+
+
+  render(){
     return (
       <MainContainer>
-        <Filtros></Filtros>
-        <Produtos></Produtos>
+        <Filtros
+        minFilter = {this.state.minFilter}
+        maxFilter = {this.state.maxFilter}
+        nameFilter = {this.state.nameFilter}
+        onChangeMinFilter = {this.onChangeMinFilter}
+        onChangeMaxFilter = {this.onChangeMaxFilter}
+        onChangeNameFilter = {this.onChangeNameFilter}
+        ></Filtros>
+        <Produtos
+        minFilter = {this.state.minFilter}
+        maxFilter = {this.state.maxFilter}
+        nameFilter = {this.state.nameFilter}
+        products = {products}
+        ></Produtos>
         <Carrinho></Carrinho>
       </MainContainer>
     
-    );
+    );}
 }
 
 
--- src/components/Produtos.jsx
+++ src/components/Produtos.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import styled from 'styled-components'
-import data from '../Data/data.json'
-
-
+import Cards from './Cards';
 
 const MainProdutos = styled.div`
   width: 56%;
@@ -15,188 +13,62 @@ const ProdutosTOP = styled.div`
  display: flex;
  align-items: center;
  justify-content: space-between;
+ padding-left: 1em;
+ padding-right: 1em;
 `
 const CardsContainer = styled.section`
   display: grid;
   grid-gap: 3%;
   flex-wrap: wrap;
-  border: 1px solid red;
   padding-top: 1em;
   padding-bottom: 3em;
+  padding-left: 1em;
   height: 100%;
   width: 100%;
-  justify-content: center;
-  justify-content: center;
-  grid-template-columns: repeat(auto-fit, 11vw)
+  justify-content: initial;
+  grid-template-columns: repeat(auto-fit, 11vw);
 `
 
-const Card1 = styled.div`
-  display: grid;
-  border-style: solid;
-  border-width: thin;
-  align-items: start;
-  text-align: center;
-  justify-items: stretch;
-
-  img{
-    width: 9em;
-    height: 9em;
-  }
-`
-
-/* const Card2 = styled.div`
-  display: grid;
-  border-style: solid;
-  border-width: thin;
-  align-items: start;
-  text-align: center;
-  justify-items: stretch;
-
-  img{
-    width: 9em;
-    height: 9em;
-  }
-`
-
-const Card3 = styled.div`
-  display: grid;
-  border-style: solid;
-  border-width: thin;
-  align-items: start;
-  text-align: center;
-  justify-items: stretch;
-
-  img{
-    width: 9em;
-    height: 9em;
-  }
-`
-
-const Card4 = styled.div`
-  display: grid;
-  border-style: solid;
-  border-width: thin;
-  align-items: start;
-  text-align: center;
-  justify-items: stretch;
-
-  img{
-    width: 9em;
-    height: 9em;
-  }
-`
-
-const Card5 = styled.div`
-  display: grid;
-
-  border-style: solid;
-  border-width: thin;
-  align-items: start;
-  text-align: center;
-  justify-items: stretch;
-
-  img{
-    width: 9em;
-    height: 9em;
-  }
-`
-
-const Card6 = styled.div`
-  display: grid;
-  border-style: solid;
-  border-width: thin;
-  align-items: start;
-  text-align: center;
-  justify-items: stretch;
-
-  img{
-    width: 9em;
-    height: 9em;
-  }
-  ` */
-
-export default class Produtos extends React.Component {
-
-    state = [
-      {
-        id: Date.now(),
-        name: 'Camiseta NASA Preta',
-        img: 'https://images2.imgbox.com/d1/e5/kF02pWQQ_o.jpg',
-        price: 'R$150,00'
-      },
-
-      {
-        id: Date.now(),
-        name: 'Camiseta NASA Branca',
-        img: 'https://images2.imgbox.com/03/70/oX7P2WyD_o.jpg',
-        price: 'R$180,00'
-      },
-
-      {
-        id: Date.now(),
-        name: 'Camiseta Gato Astronauta ',
-        img: 'https://images2.imgbox.com/1d/e4/dqrUoqDq_o.jpg',
-        price: 'R$250,00'
-      },
-
-      {
-        id: Date.now(),
-        name: 'Camiseta Planetas',
-        img: 'https://images2.imgbox.com/5a/be/IySszcVy_o.jpg',
-        price: 'R$200,00'
-      },
-
-      {
-        id: Date.now(),
-        name: 'Camiseta "I need Space"',
-        img: 'https://images2.imgbox.com/af/e2/ABdnZE87_o.jpg',
-        price: 'R$50,00'
-      },
-
-      {
-        id: Date.now(),
-        name: 'Camiseta Alien',
-        img: 'https://images2.imgbox.com/1e/9a/x6I45BAP_o.jpg',
-        price: 'R$300,00'
-      }
-    ]
 
 
-    
+ class Produtos extends React.Component {
+
+    state = {
+      sort: "CRESCENTE"
+    }
+
+    getFilteredAndOrderedList = () => {
+      return this.props.products
+        .filter((product) => product.price <= this.props.maxFilter)
+        .filter((product) => product.price >= this.props.minFilter)
+        .filter((product) => product.name.includes(this.props.nameFilter))
+        .sort((a,b) => this.state.sort === "CRESCENTE" ?a.price - b.price : b.price - a.price)
+    }
+
+    onChangeSort = (event) => {
+      this.setState({sort: event.target.value})
+    }
 
     render(){
-     const Card = (props) =>{
-        return <Card1>
-          <img src={this.props.data.img}/>
-          <p {...this.props.data.name}>{this.props.data.name}</p>
-          <p {...this.props.data.price}>{this.props.data.price}</p>
-          <button>Adicionar ao carrinho</button>
-        </Card1>
-      }
+      const filteredAndOrderedList = this.getFilteredAndOrderedList()
     return (
-
-  <MainProdutos>
-    <ProdutosTOP>
-      <p>Quantidade de produtos: {}</p>
-      <label>Ordenação:
-        <select>
-          <option value="CRESCENTE">Crescente</option>
-          <option value="DECRESCENTE">Decrescente</option>
-        </select>
-      </label>
-    </ProdutosTOP>
-    <CardsContainer>
-      {this.state
-    .map(data => {
-        return <Card key={data.id} data={data} />
-      })}
-    /* <Card1>
-        <img src={this.state[0].img} />
-        <p>{this.state[0].name}</p>
-        <p>{this.state[0].price}</p>
-        <button>Adicionar ao carrinho</button>
-      </Card1> */
-    </CardsContainer>  
-  </MainProdutos>
-  )}
+      <MainProdutos>
+        <ProdutosTOP>
+          <p>Quantidade de produtos: {filteredAndOrderedList.length}</p>
+          <label>Ordenar por:
+            <select value={this.state.sort} onChange={this.onChangeSort}>
+              <option value="CRESCENTE" selected >Crescente</option>
+              <option value="DECRESCENTE">Decrescente</option>
+            </select>
+          </label>
+        </ProdutosTOP>
+        <CardsContainer>
+          {filteredAndOrderedList.map((product) =>{
+            return <Cards product={product}/>
+          })}
+        </CardsContainer>  
+      </MainProdutos>
+    )}
   }
+
+  export default Produtos;
